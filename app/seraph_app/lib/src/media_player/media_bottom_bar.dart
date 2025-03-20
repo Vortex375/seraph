6ec5d2943a5a41a6e8402ccc9442f4c1627e9df1
Benjamin Schmitz <vortex@wolpzone.de>
2025-03-20 21:01:49 +0100

    wrap bottom bar in SafeArea

--- app/seraph_app/lib/src/media_player/media_bottom_bar.dart
+++ app/seraph_app/lib/src/media_player/media_bottom_bar.dart
@@ -12,19 +12,23 @@ class MediaBottomBar extends StatelessWidget {
     final AudioPlayerController controller = Get.find();
 
 
-    return Obx(() => !controller.open.value ? const SizedBox.shrink() : Container(
-      padding: const EdgeInsets.all(8.0),
-      color: Theme.of(context).colorScheme.surfaceContainerHighest,
-      child: Row(children: [
-          Obx(() => controller.playing.value 
-            ? IconButton.filledTonal(onPressed: controller.pause, icon: const Icon(Icons.pause))
-            : IconButton.filledTonal(onPressed: controller.play, icon: const Icon(Icons.play_arrow))),
-          const SizedBox(width: 8),
-          Expanded(child: Obx(() => Text(controller.currentMediaItem.value?.title ?? ''))),
-          const SizedBox(width: 8),
-          IconButton(onPressed: controller.closePlayer, icon: const Icon(Icons.close))
-        ]),
-    ));
+    return Obx(() => !controller.open.value ? const SizedBox.shrink() : 
+      SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(8.0),
+          color: Theme.of(context).colorScheme.surfaceContainerHighest,
+          child: Row(children: [
+            Obx(() => controller.playing.value 
+              ? IconButton.filledTonal(onPressed: controller.pause, icon: const Icon(Icons.pause))
+              : IconButton.filledTonal(onPressed: controller.play, icon: const Icon(Icons.play_arrow))),
+            const SizedBox(width: 8),
+            Expanded(child: Obx(() => Text(controller.currentMediaItem.value?.title ?? ''))),
+            const SizedBox(width: 8),
+            IconButton(onPressed: controller.closePlayer, icon: const Icon(Icons.close))
+          ]),
+        ),
+      )
+    );
   }
 
 }
