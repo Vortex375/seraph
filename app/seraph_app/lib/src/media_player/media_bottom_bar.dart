7aa14bcb28ca5a0f8486459624dbddc7fb26d822
Benjamin Schmitz <vortex@wolpzone.de>
2025-03-27 23:55:59 +0100

    add progress bar to media bottom bar to indicate playback position

--- app/seraph_app/lib/src/media_player/media_bottom_bar.dart
+++ app/seraph_app/lib/src/media_player/media_bottom_bar.dart
@@ -12,28 +12,50 @@ class MediaBottomBar extends StatelessWidget {
   Widget build(BuildContext context) {
     final AudioPlayerController controller = Get.find();
 
+    return Obx(() {
+      if (!controller.open.value) {
+        return const SizedBox.shrink();
+      }
 
-    return Obx(() => !controller.open.value ? const SizedBox.shrink() : 
-      SafeArea(
-        child: InkWell(
+      int? duration = controller.currentMediaItem.value?.duration?.inMilliseconds;
+      int position = controller.position.value.inMilliseconds;
+      double value;
+      if (duration != null) {
+        value = position.toDouble() / duration.toDouble();
+      } else {
+        value = -1;
+      }
+
+      return IntrinsicHeight(
+        child: SafeArea(
           child: Container(
-            padding: const EdgeInsets.all(8.0),
             color: Theme.of(context).colorScheme.surfaceContainerHighest,
-            child: Row(children: [
-              Obx(() => IconButton.filledTonal(
-                icon: _playButtonIcon(controller.playing.value, controller.buffering.value),
-                onPressed: controller.playing.value ? controller.pause : controller.play
-              )),
-              const SizedBox(width: 8),
-              Expanded(child: Obx(() => Text(controller.currentMediaItem.value?.title ?? ''))),
-              const SizedBox(width: 8),
-              IconButton(onPressed: controller.closePlayer, icon: const Icon(Icons.close))
-            ]),
+            child: InkWell(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(children: [
+                      Obx(() => IconButton.filledTonal(
+                        icon: _playButtonIcon(controller.playing.value, controller.buffering.value),
+                        onPressed: controller.playing.value ? controller.pause : controller.play
+                      )),
+                      const SizedBox(width: 8),
+                      Expanded(child: Obx(() => Text(controller.currentMediaItem.value?.title ?? ''))),
+                      const SizedBox(width: 8),
+                      IconButton(onPressed: controller.closePlayer, icon: const Icon(Icons.close))
+                    ]),
+                  ),
+                  if (value >= 0.0 && value <= 1.0) LinearProgressIndicator(value: value)
+                ],
+              ),
+              onTap: () => Get.toNamed(AudioPlayerView.routeName),
+            ),
           ),
-          onTap: () => Get.toNamed(AudioPlayerView.routeName),
         ),
-      )
-    );
+      );
+    });
   }
 
   Widget _playButtonIcon(bool playing, bool buffering) {
