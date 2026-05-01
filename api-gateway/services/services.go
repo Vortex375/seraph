766e9f4a836600432dec39a290b4788bed3117c8
Benjamin Schmitz <vortex@wolpzone.de>
2026-05-01 16:02:59 +0200

    feat(services): restrict services endpoint to space admins

--- api-gateway/services/services.go
+++ api-gateway/services/services.go
@@ -1,10 +1,13 @@
 package services
 
 import (
+	"errors"
 	"log/slog"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"go.uber.org/fx"
+	"umbasa.net/seraph/api-gateway/auth"
 	"umbasa.net/seraph/api-gateway/gateway-handler"
 	"umbasa.net/seraph/logging"
 	servicediscovery "umbasa.net/seraph/service-discovery"
@@ -21,6 +24,7 @@ type Params struct {
 
 	Log       *logging.Logger
 	Discovery servicediscovery.ServiceDiscovery
+	Auth      auth.Auth
 }
 
 type Result struct {
@@ -32,6 +36,7 @@ type Result struct {
 type servicesHandler struct {
 	log       *slog.Logger
 	discovery servicediscovery.ServiceDiscovery
+	auth      auth.Auth
 }
 
 func New(p Params) Result {
@@ -39,15 +44,24 @@ func New(p Params) Result {
 		Handler: &servicesHandler{
 			log:       p.Log.GetLogger("services"),
 			discovery: p.Discovery,
+			auth:      p.Auth,
 		},
 	}
 }
 
 func (h *servicesHandler) Setup(app *gin.Engine, apiGroup *gin.RouterGroup, publicApiGroup *gin.RouterGroup) {
 	apiGroup.GET("/services", func(ctx *gin.Context) {
+		if !h.auth.IsSpaceAdmin(ctx) {
+			ctx.AbortWithError(http.StatusForbidden, errors.New("only space admin can access services"))
+			return
+		}
 		ctx.JSON(200, h.discovery.Get(""))
 	})
 	apiGroup.GET("/services/type/:serviceType", func(ctx *gin.Context) {
+		if !h.auth.IsSpaceAdmin(ctx) {
+			ctx.AbortWithError(http.StatusForbidden, errors.New("only space admin can access services"))
+			return
+		}
 		ctx.JSON(200, h.discovery.Get(ctx.Param("serviceType")))
 	})
 }
